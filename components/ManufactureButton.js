632319f8dbb58d23742db564131625f54dfc9c64
Wesley-waka <wesleywaka77@gmail.com>
2024-03-05 08:26:14 +0300

    fix link reroute

--- components/ManufactureButton.js
+++ components/ManufactureButton.js
@@ -5,13 +5,13 @@ import React from 'react'
 
 const ManufactureButton = () => {
   return (
-    <a href='https://www.elitetreatforpets.com' 
+    <Link href='https://www.elitetreatforpets.com' 
     target="_blank" 
     rel="noopener noreferrer">
       <button  className='rounded-full py-1 sm:px-6  border-2 bg-burgundy hover:bg-white hover:text-amber-800 hover:border-amber-800 transition-all text-white stroke-slate-200 stroke-1 text-lg sm:w-52 w-full' style={{fontFamily: "poppins"}}>
         See More
       </button>
-    </a>
+    </Link>
   )
 }
 
