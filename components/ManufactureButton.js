2118f099233c21d3cbf8def8e083f06408f6f661
Wesley-waka <wesleywaka77@gmail.com>
2024-03-08 22:56:53 +0300

    Elite Treat Site ridirectory

--- components/ManufactureButton.js
+++ components/ManufactureButton.js
@@ -9,7 +9,7 @@ const ManufactureButton = () => {
     target="_blank" 
     rel="noopener noreferrer">
       <button  className='rounded-full py-1 sm:px-6  border-2 bg-burgundy hover:bg-white hover:text-amber-800 hover:border-amber-800 transition-all text-white stroke-slate-200 stroke-1 text-lg sm:w-52 w-full' style={{fontFamily: "poppins"}}>
-        See More
+        Visit our Website
       </button>
     </Link>
   )
