c7457a7d06f6fba481944e969ac91a7b6776f5d1
Wesley-waka <wesleywaka77@gmail.com>
2024-05-08 15:45:38 +0300

    Add web feed

--- app/layout.js
+++ app/layout.js
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export async function generateMetadata(){
   return {
   metadataBase: new URL("https://www.zemy-group.com"),
-  title: "ZEMY GROUP LLC",
-  description: "ZEMY Group LLC is a leading holding company headquartered in NY. With a spectrum of subsidiaries spanning healthcare, daily commodities, pet food, generic drugs, medical diagnostics, real estate, and hospital management consultancy, we're redefining industries and transforming possibilities.",
+  title: "ZEMY GROUP LLC - Pioneering Solutions and Sustainable Business Practice",
+  description: "ZEMY Group LLC is a leading holding company headquartered in NY. With a combination of subsidiaries spanning healthcare, daily commodities, pet food, generic drugs, medical diagnostics, real estate, and hospital management consultancy, we're redefining industries and transforming possibilities.",
   keyword: "Holding company,ZEMY Group LLC,Headquarters in NY,Subsidiaries,Daily commodities,Pet Treat,Bully sticks,Pet food,Generic drugs,Elite Treat bully sticks,Medical diagnostics,Real estate,Hospital management consultancy,Diverse portfolio,Market segments,Industry leader,Business conglomerate,Investment opportunities,Strategic partnerships,Innovation,Growth-oriented,Corporate governance",
   appleMobileWebAppTitle: 'ZEMY GROUP LLC',
   openGraph: {
-    title: "ZEMY GROUP LLC",
-    description: "Discover the diverse portfolio of ZEMY Group LLC, a leading holding company headquartered in NY. With a spectrum of subsidiaries spanning healthcare, daily commodities, pet food, generic drugs, medical diagnostics, real estate, and hospital management consultancy, we're redefining industries and transforming possibilities.",
+    title: "ZEMY GROUP LLC - Pioneering Solutions and Sustainable Business Practice",
+    description: "ZEMY Group LLC is a leading holding company headquartered in NY.With a combination of subsidiaries spanning healthcare,daily commodities,pet food,generic drugs,medical diagnostics,real estate,and hospital management consultancy.",
     url: "https://www.zemy-group.com",
     siteName: "ZEMY GROUP LLC",
     images: "https://www.zemy-group.com/logo.png",
@@ -49,6 +49,7 @@ export async function generateMetadata(){
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="alternate" type="application/rss+xml" title="ZEMY GROUP &raquo; Feed" href="https://rss.app/feeds/SRjwJEauF0ZESkkJ.xml" />
       <body className={inter.className}>{children}</body>
     </html>
   );
