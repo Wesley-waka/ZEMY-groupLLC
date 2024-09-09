5791c2361b90b99a2f81bf10b91fb0deabb73dde
wesley <wesleywaka77@gmail.com>
2024-09-09 12:23:48 +0300

    contact us

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -9,66 +9,66 @@ const page = () => {
     <div>
       <div className='flex sm:flex-row flex-col space-y-3 sm:space-y-0 sm:h-1/4 text-center sm:py-auto bg-rose-800 sm:justify-between'>
         <div className='text-white text-start mt-10 sm:mt-1/2 sm:my-auto sm:pl-14 px-3'>
-        <h1 className='font-bold'>Contact Us</h1>
-        <h2 className='font-light text-sm sm:w-10/12'>
-        Want to get in touch? We&apos;d love to hear from you. Here&apos;s how you can reach us.
-        </h2>
+          <h1 className='font-bold'>Contact Us</h1>
+          <h2 className='font-light text-sm sm:w-10/12'>
+            Want to get in touch? We&apos;d love to hear from you. Here&apos;s how you can reach us.
+          </h2>
         </div>
-        <Image src='/toll.jpg' height={1200} width={1200} className='sm:rounded-s-full sm:rounded-t-none rounded-t-full sm:h-96 h-52 sm:w-1/2 relative' alt='manufacturing-comp'/>
+        <Image src='/toll.jpg' height={1200} width={1200} className='sm:rounded-s-full sm:rounded-t-none rounded-t-full sm:h-96 h-52 sm:w-1/2 relative' alt='manufacturing-comp' />
       </div>
-      
+
       <div className='bg-gray-100'>
         <div className=' h-max flex sm:flex-row flex-col sm:space-x-8 sm:mx-40 sm:px-20 sm:py-8'>
-            <div className=' flex flex-col sm:space-y-4 space-y-2 sm:w-2/6 mx-2 sm:mx-0'>
-              <div className='bg-white flex py-4 px-2 space-x-4'>
-                <div className='p-4 bg-gray-100 rounded-full'>
-                  <IoCall className='text-burgundy' size={25}/>
-                </div>
-                <div className='text-sm flex-col space-y-1 justify-center'>
-                  <h3 className='text-sm'>Phone Number</h3>
-                  <h2 className='text-sm'>+1(917) 6094-597</h2>
-                </div>
+          <div className=' flex flex-col sm:space-y-4 space-y-2 sm:w-2/6 mx-2 sm:mx-0'>
+            <div className='bg-white flex py-4 px-2 space-x-4'>
+              <div className='p-4 bg-gray-100 rounded-full'>
+                <IoCall className='text-burgundy' size={25} />
               </div>
-              <div className='bg-white flex py-4 px-2 space-x-4'>
-                <div className='p-4 bg-gray-100 rounded-full'>
-                  <MdEmail className='text-burgundy' size={25}/>
-                </div>
-                <div className='text-sm flex flex-col space-y-1 justify-center'>
-                  <h3 className='text-sm'>Email</h3>
-                  <h2 className='text-sm'>info@zemy-group.com</h2>
-                </div>
+              <div className='text-sm flex-col space-y-1 justify-center'>
+                <h3 className='text-sm'>Phone Number</h3>
+                <h2 className='text-sm'>+1(917) 6094-597</h2>
               </div>
-              <div className='bg-white flex py-4 px-2 space-x-4 '>
-                <div className='p-4 bg-gray-100 rounded-full'>
-                  <FaLocationDot className='text-burgundy' size={25}/>
-                </div>
-                <div className='text-sm  flex-col space-y-1 justify-center'>
-                  <h3 className='text-sm'>Location</h3>
-                  <h2 className='text-sm'>USA,NewYork</h2>
-                </div>
-              </div>
-             
             </div>
-            <div className='bg-white sm:w-4/5 flex-col mx-2 p-2 my-2 sm:px-8 sm:py-10 rounded-md'>
-              <div>
-                <h3 className='text-3xl font-semibold'>Send Message</h3>
-                <h3 className='text-sm font-light'>Interested about our products and services.Get in touch with our team to learn more about ZEMY LLC</h3>
+            <div className='bg-white flex py-4 px-2 space-x-4'>
+              <div className='p-4 bg-gray-100 rounded-full'>
+                <MdEmail className='text-burgundy' size={25} />
               </div>
-              <form className='flex flex-col sm:space-y-6 space-y-4 my-2'>
-                <div className='flex sm:flex-row sm:space-x-1 space-y-4 sm:space-y-0 flex-col'>
-                  <input placeholder='First Name' className='border sm:w-1/2 rounded-lg px-2 py-2' />
-                  <input placeholder='Last Name' className='border sm:w-1/2 rounded-lg px-2 py-2'/>
-                </div>
-                <div className='flex sm:flex-row sm:space-x-1 flex-col space-y-4 sm:space-y-0'>
-                  <input placeholder='Email' className='border sm:w-1/2 rounded-lg px-2 py-2' />
-                  <input placeholder='Phone Number' className='border sm:w-1/2 rounded-lg px-2 py-2'/>
-                </div>
-                <textarea name="message" id="message" placeholder="Message"
-                    aria-required="true"
-                    required cols={30} rows={5} className='p-2 w-full rounded-lg border '/>
-                <button className='bg-blue-700 text-white rounded-md py-2 sm:w-60 w-full'>Send Message</button>
-              </form>
+              <div className='text-sm flex flex-col space-y-1 justify-center'>
+                <h3 className='text-sm'>Email</h3>
+                <h2 className='text-sm'>info@zemy-group.com</h2>
+              </div>
             </div>
+            <div className='bg-white flex py-4 px-2 space-x-4 '>
+              <div className='p-4 bg-gray-100 rounded-full'>
+                <FaLocationDot className='text-burgundy' size={25} />
+              </div>
+              <div className='text-sm  flex-col space-y-1 justify-center'>
+                <h3 className='text-sm'>Location</h3>
+                <h2 className='text-sm'>USA, NewYork</h2>
+              </div>
+            </div>
+
+          </div>
+          <div className='bg-white sm:w-4/5 flex-col mx-2 p-2 my-2 sm:px-8 sm:py-10 rounded-md'>
+            <div>
+              <h3 className='text-3xl font-semibold'>Send Message</h3>
+              <h3 className='text-sm font-light'>Interested about our products and services.Get in touch with our team to learn more about ZEMY LLC</h3>
+            </div>
+            <form className='flex flex-col sm:space-y-6 space-y-4 my-2'>
+              <div className='flex sm:flex-row sm:space-x-1 space-y-4 sm:space-y-0 flex-col'>
+                <input placeholder='First Name' className='border sm:w-1/2 rounded-lg px-2 py-2' />
+                <input placeholder='Last Name' className='border sm:w-1/2 rounded-lg px-2 py-2' />
+              </div>
+              <div className='flex sm:flex-row sm:space-x-1 flex-col space-y-4 sm:space-y-0'>
+                <input placeholder='Email' className='border sm:w-1/2 rounded-lg px-2 py-2' />
+                <input placeholder='Phone Number' className='border sm:w-1/2 rounded-lg px-2 py-2' />
+              </div>
+              <textarea name="message" id="message" placeholder="Message"
+                aria-required="true"
+                required cols={30} rows={5} className='p-2 w-full rounded-lg border ' />
+              <button className='bg-blue-700 text-white rounded-md py-2 sm:w-60 w-full'>Send Message</button>
+            </form>
+          </div>
         </div>
       </div>
 
--- components/Service.js
+++ components/Service.js
@@ -10,29 +10,29 @@ const Service = () => {
   return (
     <div className={styles.section}>
       <h2 className="text-center sm:py-4 mx-auto  font-bold ">WHO WE ARE</h2>
-    <div className='sm:ml-40 sm:m-6 mx-4 xs:mt-5'>
-      <article className='header flex flex-col sm:flex-row sm:space-x-8 sm:my-7 text-burgundy xs:my-4'>
-        <h1 className='font-bold text-3xl'>
-        ZEMY Group  LLC, <br /> 
-        </h1>
-        <p className='sm:w-5/12'>
-        is a rich portfolio enveloping healthcare, daily commodities, real estate, and more, we are the nexus of industry-leading subsidiaries making a global impact.
-        </p>
-      </article>
-      <div className={`flex sm:flex-row flex-col  h-3/4 sm:space-x-6 space-y-6 sm:space-y-0 w-full  mx-auto sm:align-middle`}>
-        {services.map((service) => {
-          const { id, icon, title, text } = service
-          return (
-            <article className={styles.service} key={id}>
-              <span className={styles.icon}>{icon}</span>
-              <h4 className='text-burgundy font-semibold'>{title}</h4>
-              <p className='text-burgundy'>{text}</p>
-            </article>
-          )
-        })}
+      <div className='sm:ml-40 sm:m-6 mx-4'>
+        <article className='header flex flex-col sm:flex-row sm:space-x-8 sm:mb-7 text-burgundy xs:mb-4'>
+          <h1 className='font-bold text-3xl'>
+            ZEMY Group  LLC, <br />
+          </h1>
+          <p className='sm:w-5/12'>
+            is a rich portfolio enveloping healthcare, daily commodities, real estate, and more, we are the nexus of industry-leading subsidiaries making a global impact.
+          </p>
+        </article>
+        <div className={`flex sm:flex-row flex-col  h-3/4 sm:space-x-6 space-y-6 sm:space-y-0 w-full  mx-auto sm:align-middle`}>
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+            return (
+              <article className={styles.service} key={id}>
+                <span className={styles.icon}>{icon}</span>
+                <h4 className='text-burgundy font-semibold'>{title}</h4>
+                <p className='text-burgundy'>{text}</p>
+              </article>
+            )
+          })}
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
